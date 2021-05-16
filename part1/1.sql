04915d1f6417af2e63dc53096eae1cf533446e85
ElizarevaNatalia <77670474+ElizarevaNatalia@users.noreply.github.com>
2021-05-16 20:39:10 +0300

    Add files via upload

---
+++ part1/1.sql
@@ -0,0 +1,8 @@
+﻿with conn.cursor() as cur:
+    cur.execute("""SELECT AVG(empty_seats) as empty_seats FROM (
+        SELECT (count(s.seat_no) - (SELECT count(seat_no) FROM Boarding_passes AS bp WHERE bp.flight_id=f.flight_id)) AS empty_seats
+        FROM Flights AS f
+        INNER JOIN Seats AS s ON f.aircraft_code = s.aircraft_code
+        GROUP BY f.flight_id
+    ) as name""")
+    print(DF(cur.fetchall()))
