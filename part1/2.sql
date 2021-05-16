8eaaff9e8442f12f00037f57938e63f7cbd9effb
ElizarevaNatalia <77670474+ElizarevaNatalia@users.noreply.github.com>
2021-05-16 20:41:58 +0300

    Add files via upload

---
+++ part1/2.sql
@@ -0,0 +1,7 @@
+﻿with conn.cursor() as cur:
+    cur.execute("""SELECT f.aircraft_code, AVG(f.actual_arrival - f.actual_departure) as average_flight_duration
+                   FROM flights AS f
+                   GROUP BY f.aircraft_code
+                   ORDER BY average_flight_duration DESC
+                   LIMIT 3""")
+    print(DF(cur.fetchall()))
